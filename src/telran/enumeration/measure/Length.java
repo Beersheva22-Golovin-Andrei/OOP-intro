09ea431dec91d7c8857ff7f46b800ad3bb464a80
GolovinAndrei <golovinandrei@tut.by>
2023-02-13 21:54:23 +0200

    HW#30B

--- src/telran/enumeration/measure/Length.java
+++ src/telran/enumeration/measure/Length.java
@@ -4,18 +4,24 @@ public class Length implements Comparable<Length>{
 
 	private float amount;
 	private LengthUnit lengthUnit;
+	
 	public Length(float amount, LengthUnit lengthUnit) {
 		this.amount = amount;
 		this.lengthUnit = lengthUnit;
 	}
+	
 	@Override
 	/**
 	 * equals two Length objects according to LengthUnit and amount
 	 * 10m == 10000mm
 	 */
 	public boolean equals(Object obj) {
-		//TODO
-		return false;
+		boolean res = false;
+		if (obj instanceof Length) {
+			Length comparebleLength = ((Length)obj).convert(lengthUnit);
+			res = comparebleLength.getAmount()==amount;
+		}
+		return res;
 	}
 
 	@Override
@@ -27,9 +33,20 @@ public class Length implements Comparable<Length>{
 	 *  == 0 "this" object equals "o" object,
 	 */
 	public int compareTo(Length o) {
-		//TODO
-		return -1;
+		if (o.getUnit()!=lengthUnit) {
+			o = o.convert(lengthUnit);
+		}
+		float temp = amount-o.amount;
+		int res = 0;
+		if (temp < 0) {
+			res = -1;
+		} else if (temp > 0) {
+			res = 1;
+		}
+		return res;
+		
 	}
+	
 	/**
 	 * 
 	 * @param unit
@@ -37,18 +54,16 @@ public class Length implements Comparable<Length>{
 	 * example, convert(LengthUnit.M) returns Length in meters 
 	 */
 	public Length convert(LengthUnit unit) {
-		//TODO
-		
-		return null;
+		return new Length(lengthUnit.value * amount / unit.value, unit);
 	}
+	
 	@Override
 	/**
 	 * returns string with amount and length unit pinned to amount with no space
 	 * Example: 20.0M (string expression of Length object presenting 20 meters)
 	 */
 	public String toString() {
-		//TODO
-		return null;
+		return ""+amount+lengthUnit.name();
 	}
 
 	public float getAmount() {
--- src/telran/enumeration/measure/LengthUnit.java
+++ src/telran/enumeration/measure/LengthUnit.java
@@ -3,16 +3,21 @@ package telran.enumeration.measure;
 public enum LengthUnit {
 
 	MM(1), CM(10), IN(25.4f), M(1000),KM(1_000_000);
+	
 	float value;
+	
 	LengthUnit(float value) {
 		this.value = value;
 	}
+	
 	public Length between(Length l1, Length l2) {
-		//TODO
-		//returns Length object as length between l1 and l2 in "this" units
-		//Example: LengthUnit.M.between (new Length(200, LengthUnit.CM), new Length(1, LengthUnit.M))
-		//returns Length(1, LengthUnit.M)
-		return null;
+		if (this != l1.getUnit()) {
+			l1=l1.convert(this);
+		}
+		if (this != l2.getUnit()) {
+			l2=l2.convert(this);
+		}		
+		return new Length(Math.abs(l2.getAmount()-l1.getAmount()), this);
 	}
 	public float getValue() {
 		return value;
